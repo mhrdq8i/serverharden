67e204ce19d2e342f40a7f094f4e7dc748abd98a
Mehrdad <40826835+mehrdad-drpc@users.noreply.github.com>
2023-06-19 14:44:37 -0700

    Update harden_proxy_server.sh
    fix UFW function
    add ssh port number selector

--- harden_proxy_server.sh
+++ harden_proxy_server.sh
@@ -10,6 +10,43 @@ YELLOW='\033[1;33m'
 PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 
+# define functions
+function fn_yes {
+    rm -f /etc/ssh/sshd_config
+    cp  ${PWD}/sshd_config /etc/ssh/sshd_config
+    sleep 1
+    printf "${GREEN}config file has been copied ${NC}\n"
+    systemctl restart ssh
+    printf "${PURPLE}ssh service status${NC}\n"
+    systemctl status ssh
+    printf "${PURPLE}ssh port(s) status${NC}\n"
+    ss -tlpn | grep sshd
+}
+
+function fn_ufw {
+    ssh_pn=`cat /etc/ssh/sshd_config | grep "Port " | cut -d" " -f2`
+    printf "ssh port number is ${PURPLE}$ssh_pn${NC}\n"
+    sleep 3
+    ufw allow $ssh_pn
+    printf "${RED_BLINK}ENABLING UFW !!!${NC}\n"
+    sleep 3
+    ufw enable
+    sleep 3
+    ufw status
+}
+
+function fn_no {
+    printf "${PURPLE}Go back and return after copy, I'm wating for you :) ${NC}\n"
+    printf "${BLACK}HINT: ${NC}'ssh-copy-id -p $ssh_pn -i ./id_rsa.pub ${user}@<your-server-addr>' \n"
+    sleep 10
+    printf "${RED_BLINK}did you copy sshkey? ${NC}\n"
+    read -p "[ yes | no ] or 'e' to exit: " answer
+}
+
+function fn_else {
+    read -p "usage: just [ yes | no ] or 'e' to exit enter again: " answer
+}
+
 # check user is root or sudoer or not
 if [ "$EUID" -ne 0 ]; then
     printf "${YELLOW}please run as root's privilege ${NC}\n"
@@ -35,34 +72,36 @@ usermod -aG sudo $user
 usermod --lock root
 
 # create custom ssh banner
-apt update -y && apt install -y figlet
-figlet drsrv > /etc/ssh/custom_banner
+# apt update -y && apt install -y figlet
+# figlet drsrv > /etc/ssh/custom_banner
 
 # change ssh defult config [port, root login, restrict login with password]
 printf "${RED}did you copy sshkey for new user!? ${NC}\n"
-read -p "yes or no: " answer
+read -p "[ yes | no ] or 'e' to exit: " answer
 
-while [ $answer != "" ]; do
-    if [ $answer = "yes" ]; then
-        rm -f /etc/ssh/sshd_config
-        cp  ${PWD}/sshd_config /etc/ssh/sshd_config
-        sleep 1
-        printf "${GREEN}config file has been copied ${NC}\n"
-        systemctl restart ssh
-        printf "${PURPLE}ssh service status${NC}\n"
-        systemctl status ssh
-        printf "${PURPLE}ssh port(s) status${NC}\n"
-        ss -tlpn | grep sshd
-        exit 0
-    elif [ $answer = "no" ]; then
-        printf "${PURPLE}go back and return after copy, I'm wating for you :) ${NC}\n"
-        printf "${BLACK}HINT: ${NC}'ssh-copy-id -p 8452 -i ./id_rsa.pub ${user}@<your-server-addr>' \n"
-        sleep 10
-        printf "${RED_BLINK}did you copy sshkey? ${NC}\n"
-        read -p "yes or no: " answer
-        continue
-    else
-        read -p "usage: just 'yes' or 'no' enter again: " answer
-        continue
-    fi
+while [ true ]; do
+    case $answer in
+        yes)
+            fn_yes
+            fn_ufw
+            cat /etc/ssh/custom_banner
+            exit 0
+        ;;
+        no)
+            fn_no
+            continue
+        ;;
+        e)
+            
+            printf "${GREEN}BYE! ${NC}\n"
+            exit 0
+        ;;
+        *)
+            fn_else
+            continue
+            
+        ;;
+    esac
 done
+
+
