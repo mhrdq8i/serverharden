a89b630873f1da35dbfc34be7535c51906a832c4
Mehrdad Qasemkhani <mehrdad.pc@gmail.com>
2023-08-22 12:48:51 +0000

    change kolli chiz miz :)

--- hardening.sh
+++ hardening.sh
@@ -10,6 +10,13 @@ YELLOW='\033[1;33m'
 PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 
+
+# check user is root or sudoer or not
+if [ "$EUID" -ne 0 ]; then
+    printf "${YELLOW}please run as root's privilege ${NC}\n"
+    exit 1
+fi
+
 # define functions
 function fn_yes {
     rm -f /etc/ssh/sshd_config
@@ -17,15 +24,15 @@ function fn_yes {
     sleep 1
     printf "${GREEN}config file has been copied ${NC}\n"
     systemctl restart ssh
-    printf "${PURPLE}ssh service status${NC}\n"
-    systemctl status ssh
+    printf "${PURPLE}ssh service status${NC}\t"
+    systemctl status sshd | awk 'NR==2{ print $4 }' | tr --delete ";"
     printf "${PURPLE}ssh port(s) status${NC}\n"
-    ss -tlpn | grep sshd
+    ss -tulpn | grep ssh | head --lines 1
 }
 
 function fn_ufw {
-    sed -i s/IPV6=yes/IPV6=no/ /etc/default/ufw
-    ssh_pn=`cat /etc/ssh/sshd_config | grep "Port " | cut -d" " -f2`
+    sed --in-place s/IPV6=yes/IPV6=no/ /etc/default/ufw
+    ssh_pn=`cat /etc/ssh/sshd_config | grep "Port " | cut --delimiter=" " -f2`
     printf "ssh port number is ${PURPLE}$ssh_pn${NC}\n"
     sleep 3
     ufw allow $ssh_pn
@@ -48,36 +55,48 @@ function fn_else {
     read -p "usage: just [ yes | no ] or 'e' to exit enter again: " answer
 }
 
-function fn_cpnusr_lrusr {
-    # check user is root or sudoer or not
-    if [ "$EUID" -ne 0 ]; then
-        printf "${YELLOW}please run as root's privilege ${NC}\n"
-        exit 1
-    fi
-
+function fn_create_promot_new_user {
+    
+    printf "${GREEN}create a non-root user to increase your security!!! ${NC}\n"
     # delete duplicate user & create & promote new user
     read -p "please enter a user's name: " user
-    ## TODO: add if then active userdel
-    # userdel -r $user
-    useradd -m $user --shell /bin/bash
-    mkdir -p $user/.ssh
-
+    id $user > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        printf "${RED}user [$user] is exists ${NC}\n"
+        userdel --remove $user
+        useradd --create-home $user --shell /bin/bash
+        mkdir --parents $user/.ssh
+        rm -rf ./$user
+        printf "${GREEN}user [$user] was deleted & created clearly ${NC}\n"
+        sleep 2
+    else
+        useradd --create-home $user --shell /bin/bash
+        mkdir --parents $user/.ssh
+        rm -rf ./$user
+        printf "${GREEN}user [$user] was created ${NC}\n"
+        sleep 2
+    fi
+    
+    
     # set a new password for user
     passwd $user
     while [ $? -ne 0 ];do
         printf "please enter the new user's password below \n"
         passwd $user
     done
-
+    
     # add the new user to sudeor group
     usermod -aG sudo $user
+    
+}
 
+function fn_lock_root_login {
     # lock root login
     usermod --lock root
 }
 
 # create a custom ssh banner
-printf "${PURPLE}running some background commands, please be patient ${NC} \n"
+printf "${PURPLE}running some background tasks, please be patient ${NC} \n"
 apt update -y > /dev/null 2>&1
 apt install -y figlet ufw > /dev/null 2>&1
 figlet drsrv > /etc/ssh/custom_banner
@@ -85,8 +104,15 @@ printf "${YELLOW}ssh banner has been configured ${NC} \n"
 printf "${YELLOW}apt update & install has been done ${NC} \n"
 
 
-# run fn create & promot new user & lock root user
-fn_cpnusr_lrusr
+# run fn create & promot new user
+fn_create_promot_new_user
+
+# lock root login
+read -p "would you like to disable root login? [ yes | no ]: " rl_answr
+if [ $rl_answr  = "yes" ]; then
+    fn_lock_root_login
+fi
+
 
 # change ssh default config [port, root login, restrict login with password]
 printf "${RED}did you copy sshkey for new user!? ${NC}\n"
@@ -106,13 +132,14 @@ while [ true ]; do
         ;;
         e)
             userdel -r $user
+            printf "${RED}The user $user has been removed${NC}\n"
             printf "${GREEN}BYE! ${NC}\n"
             exit 0
         ;;
         *)
             fn_else
             continue
-
+            
         ;;
     esac
 done
