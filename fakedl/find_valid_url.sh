dd2dbba6697f048a4ea46d7e71d37391fafa793e
Mehrdad Qasemkhani <mehrdad.pc@gmail.com>
2024-02-15 18:54:35 +0330

    delete a file

---
+++ fakedl/find_valid_url.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+curl_req() {
+    local vurl="$1"
+    local req=$(curl -scL "$vurl" -o /tmp/`date +%A%d%b%y-%H%M%S`)
+}
+
+# Function to check if a URL is valid
+is_valid_url() {
+    local url="$1"
+    local http_code=`curl -Is "$url" -o /dev/null -w '%{http_code}'`
+    [[ "$http_code" -eq 200 || "$http_code" -eq 302 ]]
+}
+
+# Read the file line by line and search for a valid URL
+while IFS= read -r url; do
+    if is_valid_url "$url"; then
+	valid_url="$url"
+	echo "valid URL is: $valid_url"
+	curl_req "$valid_url"
+        exit 0
+    elif ! is_valid_url "$url"; then
+	continue	
+    fi
+done < "urls.txt"
+
+echo "No valid URL found"
+
